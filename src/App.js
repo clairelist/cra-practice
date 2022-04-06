b24bbc33f86319dc8a6360c1d09c8dc59408f408
Claire List <claire.list2501@gmail.com>
2022-04-06 10:11:20 -0600

    remote repo setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className='nav-wrapper'>
+        <Link to='/user'>YOUR page</Link>
+        <Link to='/home'>YOUR home</Link>
+        <Link to='/post'>POST some NONSENSE</Link>
+        </nav>
       </header>
     </div>
   );
