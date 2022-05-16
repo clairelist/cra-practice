1063fd9838b99f5eef760138963df1e2f2000915
Claire List <claire.list2501@gmail.com>
2022-05-16 11:57:55 -0600

    post wip

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,30 @@
+import {useState} from 'react';
+
+const defaultPost = {
+    title : '',
+    content: ''
+}; 
 
 function Post(){
+    const [post, setPost] = useState(defaultPost);
+
+    const handleChange = (args) =>{
+        setPost(args);
+    }
     return(
         <div>
             <h2>POST component!</h2>
+<form>
+        <label>
+        Title
+        <input type="text" name="title" />
+        </label>
+
+        <label>
+        <input type="textarea" name="content" />
+        </label>
+        
+</form>
         </div>
     )
 }
