2351a6f38a15f9e051c68017c048c8214df6db62
Claire List <claire.list2501@gmail.com>
2022-06-02 21:18:54 -0600

    POST styles

--- src/Post.js
+++ src/Post.js
@@ -3,34 +3,51 @@ import {useState} from 'react';
 import './Post.css';
 
 const defaultPost = {
-    title: '',
-    content: ''
+    title: 'DEFAULT TITLE!',
+    content: 'DEFAULT CONTENT!'
 }; 
 
 function Post(){
     const [post, setPost] = useState(defaultPost); //...we will POST post data from here later ! So we can persist in everyone's favorite -- the database woooo
 
     const handleChange = (event) =>{
-        setPost({value: event.target.value});
+        setPost(event.target.value);
     }
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) =>{ //call!
         event.preventDefault();
+        setPost(event.post); //this is not correct LOL
     }
     return(
         <div>
             <h2>POST component!</h2>
-<form>
+<form className='text-input'>
         <label>
-        Title
-        <input type="text" name="title" />
+        Title _
+        <input 
+        type="text" 
+        name="title" 
+         />
         </label>
 
-        <label>
-        <input type="textarea" name="content" rows={200} cols={25} />
-        </label>
+        {/* <label>
+            Content _
+        <input 
+        type="textarea" 
+        name="textarea" 
+        rows={200} 
+        cols={25} 
+        value={post}
+        onChange={handleChange}
+       
+        />
+        </label> */}
+
+        <textarea placeholder={'post some nonsense!'}></textarea>
         
 </form>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
         </div>
     )
 }
