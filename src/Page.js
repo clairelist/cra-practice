edec85c5588f9acbb172a5e1d9c969b3e3209332
Claire List <claire.list2501@gmail.com>
2022-07-21 09:31:07 -0600

    'WIP'

--- src/Page.js
+++ src/Page.js
@@ -1,8 +1,12 @@
+import { useState, useEffect } from 'react';
 
-function Page(){
+//I need to: get user info on mount of this component and display it. Also to include in stretch goals:: ability to edit this page 
+function Page(props){
+  
     return(
         <div>
-            <h2>PAGE component!</h2>
+            <h2 className='page-viewer-header'>Your PROFILE page!</h2>
+            <p>This is my profile page it has information about me as a person.</p>
         </div>
     )
 }
