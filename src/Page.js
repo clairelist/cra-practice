8a1ae1949b614e6abf55d8400581501ef5144094
Claire List <claire.list2501@gmail.com>
2022-07-21 11:42:16 -0600

    'WIP'

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from 'react';
 
 //I need to: get user info on mount of this component and display it. Also to include in stretch goals:: ability to edit this page 
-function Page(props){
-  
+function Page(){
+    const defaultUser = {
+        username: 'USERNAME',
+        description: 'DESCRIPTION IS A LONGER STRING WOOOOOO',
+        role: 'choice bt user, moderator, admin, superadmin',
+    }
+    const [user,setUser] = useState(defaultUser);
     return(
         <div>
-            <h2 className='page-viewer-header'>Your PROFILE page!</h2>
-            <p>This is my profile page it has information about me as a person.</p>
+            <h2 className='profile-viewer-header'>Welcome to {user.username}'s page!</h2>
+            <p className='profile-description'>{user.description}</p>
+            <h3 className='profile-role'>{user.username} is a(n) {user.role}</h3>
         </div>
     )
 }
